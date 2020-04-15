f88516bd585a7e83cce87e387d34923a204fe869
Daniel Kiss <kissd621@gmail.com>
2020-04-15 11:40:19 +0200

    Forgotten password UI redesigned

--- src/app/auth/auth-material.module.ts
+++ src/app/auth/auth-material.module.ts
@@ -3,12 +3,16 @@ import { MatCardModule} from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 const material = [
   MatCardModule,
   MatButtonModule,
   MatFormFieldModule,
-  MatInputModule
+  MatInputModule,
+  MatProgressBarModule,
+  MatSnackBarModule
 ];
 
 @NgModule({
--- src/app/auth/forgotten-password/auth-forgotten-password.component.ts
+++ src/app/auth/forgotten-password/auth-forgotten-password.component.ts
@@ -2,20 +2,30 @@ import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-auth-forgotten-password',
   templateUrl: './auth-forgotten-password.component.html',
+  styleUrls: ['./auth-forgotten-password.component.css'],
 })
 export class AuthForgottenPasswordComponent {
   loading;
-  alertMsg;
 
-  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {}
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
 
   resetPasswordForm = this.fb.group({
     email: this.fb.control(null, [Validators.required, Validators.email]),
   });
+
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, { duration: 5000 });
+  }
 
   onSendResetPassword() {
     this.loading = true;
@@ -24,24 +34,17 @@ export class AuthForgottenPasswordComponent {
     this.authService
       .sendPasswordResetEmail(email)
       .then(() => {
-        this.alertMsg = {
-          color: 'alert-success',
-          message: `Password reset email successfully sent to ${email}`,
-        };
+        this.openSnackBar(`Password reset email successfully sent to ${email}!`, null);
       })
-      .catch((err) => {
-        this.alertMsg = {
-          color: 'alert-danger',
-          message: `Password reset email couldn't be sent.`,
-        };
+      .catch(() => {
+        this.openSnackBar("Password reset email couldn't be sent. Please try again!", null);
       })
       .finally(() => {
         this.loading = false;
 
         setTimeout(() => {
-          this.alertMsg = null;
           this.router.navigate(['../auth/login']);
-        }, 8000);
+        }, 3000);
       });
   }
 }
